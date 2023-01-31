4742460490da51f3203fd07ef558c85ffb886e03
Yevgeniy Starinets <gobvoz@gmail.com>
2023-01-30 23:26:48 -0500

    - change `webpack.config.js` to `webpack.config.ts`

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,8 +1,8 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+import path from 'path';
+import webpack from 'webpack';
+import HTMLWebpackPlugin from 'html-webpack-plugin';
 
-module.exports = {
+const config: webpack.Configuration = {
   mode: 'development',
   entry: path.resolve(__dirname, 'src', 'index.ts'),
   output: {
@@ -29,3 +29,5 @@ module.exports = {
     extensions: ['.tsx', '.ts', 'js'],
   },
 };
+
+export default config;
