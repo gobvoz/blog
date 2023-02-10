942ee46edec4fac1f2c68ceddd8bfbc82ebba943
Yevgeniy Starinets <gobvoz@gmail.com>
2023-02-10 00:02:23 -0500

    - fix `double space` class string if parent don't send a class name

--- src/shared/libs/class-names.ts
+++ src/shared/libs/class-names.ts
@@ -2,7 +2,7 @@ type Mods = Record<string, boolean | string>;
 
 export const classNames = (classNameList: string[], mods: Mods = {}): string => {
   return [
-    ...classNameList,
+    ...classNameList.filter(Boolean),
     Object.entries(mods)
       .filter(([_, value]) => Boolean(value))
       .map(([className, _]) => className),
