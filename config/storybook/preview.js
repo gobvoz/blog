5ce1ada1aad2cdd85ae873002ab71a9aeea864c3
Yevgeniy Starinets <gobvoz@gmail.com>
2023-03-19 21:50:14 -0400

    - fix `storybook` translations (solve `i18n` translation issue), add static dirs to `storybook` config (to access translations)

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,7 +1,8 @@
 import { addDecorator } from '@storybook/react';
+
 import { RouterDecorator } from 'shared/config/storybook/router-decorator';
 import { StoreDecorator } from 'shared/config/storybook/store-decorator';
-
+import { TranslationDecorator } from 'shared/config/storybook/translation-decorator';
 import { StyleDecorator } from 'shared/config/storybook/style-decorator';
 import { ThemeDecorator } from 'shared/config/storybook/theme-decorator';
 import { Theme } from 'shared/constants/theme';
@@ -20,3 +21,4 @@ addDecorator(StoreDecorator);
 addDecorator(StyleDecorator);
 addDecorator(ThemeDecorator(Theme.LIGHT));
 addDecorator(RouterDecorator);
+addDecorator(TranslationDecorator);
