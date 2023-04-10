1fc7d8c91c30b9928183166bbf14222ca040a7bc
Yevgeniy Starinets <gobvoz@gmail.com>
2023-04-09 21:10:28 -0400

    - add `test-async-thunk` to shared libs - refactor `login-by-user-name` tests to use `test-async-thunk`

--- src/features/auth-by-user-name/model/services/__tests__/login-by-user-name.test.ts
+++ src/features/auth-by-user-name/model/services/__tests__/login-by-user-name.test.ts
@@ -1,22 +1,17 @@
 import axios from 'axios';
 
-import { loginByUsername } from '../login-by-user-name';
 import { userActions } from 'entities/user';
+
+import { TestAsyncThunk } from 'shared/libs/tests/test-async-thunk';
+
+import { loginByUsername } from '../login-by-user-name';
 
 jest.mock('axios');
 
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('loginByUserName', () => {
-  let dispatch: jest.Mock;
-  let getState: jest.Mock;
-
-  beforeEach(() => {
-    dispatch = jest.fn();
-    getState = jest.fn();
-  });
-
-  it('', async () => {
+  it('should to be a successful login', async () => {
     const userData = { username: 'username', password: 'password' };
     const returnedData = { username: 'username', id: '123' };
 
@@ -26,17 +21,16 @@ describe('loginByUserName', () => {
       }),
     );
 
-    const action = loginByUsername(userData);
-    const result = await action(dispatch, getState, undefined);
+    const thunk = new TestAsyncThunk(loginByUsername);
+    const result = await thunk.dispatchAsync(userData);
 
-    expect(dispatch).toHaveBeenCalledTimes(3);
-    expect(dispatch).toHaveBeenCalledWith(userActions.setAuthData(returnedData));
+    expect(thunk.dispatch).toHaveBeenCalledTimes(3);
+    expect(thunk.dispatch).toHaveBeenCalledWith(userActions.setAuthData(returnedData));
     expect(mockedAxios.post).toHaveBeenCalled();
     expect(result.meta.requestStatus).toEqual('fulfilled');
     expect(result.payload).toEqual(returnedData);
   });
-
-  it('', async () => {
+  it('should to be rejected', async () => {
     const userData = { username: 'username', password: 'password' };
     const returnedData = 401;
 
@@ -46,10 +40,10 @@ describe('loginByUserName', () => {
       }),
     );
 
-    const action = loginByUsername(userData);
-    const result = await action(dispatch, getState, undefined);
+    const thunk = new TestAsyncThunk(loginByUsername);
+    const result = await thunk.dispatchAsync(userData);
 
-    expect(dispatch).toHaveBeenCalledTimes(2);
+    expect(thunk.dispatch).toHaveBeenCalledTimes(2);
     expect(mockedAxios.post).toHaveBeenCalled();
     expect(result.meta.requestStatus).toEqual('rejected');
     expect(result.payload).toEqual(undefined);
---
+++ src/shared/libs/tests/test-async-thunk.ts
@@ -0,0 +1,21 @@
+import { AsyncThunkAction } from '@reduxjs/toolkit';
+
+type ActionCreatorType<Return, Arg, RejectedValue> = (
+  arg: Arg,
+) => AsyncThunkAction<Return, Arg, { rejectValue: RejectedValue }>;
+
+export class TestAsyncThunk<Return, Arg, RejectedValue> {
+  dispatch: jest.MockedFn<any> = jest.fn();
+  getState: jest.MockedFn<any> = jest.fn();
+  actionCreator: ActionCreatorType<Return, Arg, RejectedValue>;
+
+  constructor(actionCreator: ActionCreatorType<Return, Arg, RejectedValue>) {
+    this.actionCreator = actionCreator;
+  }
+
+  async dispatchAsync(arg: Arg) {
+    const action = this.actionCreator(arg);
+    const result = await action(this.dispatch, this.getState, undefined);
+    return result;
+  }
+}
