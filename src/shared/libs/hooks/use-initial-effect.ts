13b770df20627899fc1ec31672855fb5f67dd1df
Yevgeniy Starinets <gobvoz@gmail.com>
2024-02-18 22:09:03 -0500

    - add `asyncReducers` to `renderComponent` for testing - fix `useInitialEffect` to run tests

--- src/shared/libs/hooks/use-initial-effect.ts
+++ src/shared/libs/hooks/use-initial-effect.ts
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 // TODO: Rename it to useAppEffect
 const useInitialEffect = (callback: () => void, dependencies: Array<unknown> = []) => {
   useEffect(() => {
-    if (__PROJECT__ !== 'storybook') callback();
+    if (__PROJECT__ !== 'storybook' && __PROJECT__ !== 'jest') callback();
   }, dependencies);
 };
 
--- src/shared/libs/tests/component-render.tsx
+++ src/shared/libs/tests/component-render.tsx
@@ -1,24 +1,26 @@
 import { ReactNode } from 'react';
 import { render } from '@testing-library/react';
+import { ReducersMapObject } from '@reduxjs/toolkit';
 import { I18nextProvider } from 'react-i18next';
 import { MemoryRouter } from 'react-router-dom';
 
 import { StateSchema, StoreProvider } from 'app/providers/store-provider';
+import { CollapseProvider } from 'app/providers/collapse-provider';
 
 import i18n from 'shared/config/i18n/i18n.for-tests';
-import { CollapseProvider } from 'app/providers/collapse-provider';
 
 export interface ComponentRenderOptions {
   route?: string;
   initialState?: DeepPartial<StateSchema>;
+  asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>;
 }
 
 export const componentRender = (component: ReactNode, options: ComponentRenderOptions = {}) => {
-  const { route = '/', initialState } = options;
+  const { route = '/', initialState, asyncReducers } = options;
 
   return render(
     <MemoryRouter initialEntries={[route]}>
-      <StoreProvider initialState={initialState as StateSchema}>
+      <StoreProvider initialState={initialState as StateSchema} asyncReducers={asyncReducers}>
         <CollapseProvider initialValue={false}>
           <I18nextProvider i18n={i18n}>{component}</I18nextProvider>
         </CollapseProvider>
